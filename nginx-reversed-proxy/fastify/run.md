740317d974bc9008c06a19af7a50fe18a95840d7
Satit Rianpit <siteslave@users.noreply.github.com>
2023-01-30 06:37:59 +0700

    Add environment

--- nginx-reversed-proxy/fastify/run.md
+++ nginx-reversed-proxy/fastify/run.md
@@ -7,5 +7,17 @@ sh build.sh
 # run
 
 ```shell
+DB_HOST=localhost \
+DB_PORT=5432 \
+DB_USER=postgres \
+DB_PASS=789124 \
+DB_NAME=test \
+SERVER_NAME=DEMO1 \
+node services.js
+```
+
+# docker run 
+
+```shell
 docker run -p 3001:3000 -e TEST_MESSAGE=doraemon r7platform/fastify-demo
 ```
--- nginx-reversed-proxy/fastify/services.js
+++ nginx-reversed-proxy/fastify/services.js
@@ -11,8 +11,14 @@ fastify.addHook('onSend', (request, reply, playload, next) => {
   next();
 });
 
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_PORT = process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432
+const DB_USER = process.env.DB_USER || 'postgres'
+const DB_PASS = process.env.DB_PASS || ''
+const DB_NAME = process.env.DB_NAME || ''
+
 fastify.register(require('@fastify/postgres'), {
-  connectionString: 'postgres://postgres:789124@localhost/test'
+  connectionString: `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 })
 
 fastify.get('/', async (request, reply) => {
